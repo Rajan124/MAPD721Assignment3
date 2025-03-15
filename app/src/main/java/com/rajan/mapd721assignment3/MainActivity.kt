fb672274a346a95d9db5edbe3e66dedd5254d589
Rajan <boudelrajan@gmail.com>
2025-03-15 01:08:53 -0400

    Changed in main activity and created Animation1 Screen

--- app/src/main/java/com/rajan/mapd721assignment3/MainActivity.kt
+++ app/src/main/java/com/rajan/mapd721assignment3/MainActivity.kt
@@ -3,45 +3,62 @@ package com.rajan.mapd721assignment3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import com.rajan.mapd721assignment3.ui.theme.MAPD721Assignment3Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MAPD721Assignment3Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            AppNavigation()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainMenu(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Main Menu", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(20.dp))
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MAPD721Assignment3Theme {
-        Greeting("Android")
+        Button(onClick = { navController.navigate("animation1") }) {
+            Text("Animation 1 Demo")
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Button(onClick = { navController.navigate("animation2") }) {
+            Text("Animation 2 Demo")
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Button(onClick = { navController.navigate("animation3") }) {
+            Text("Animation 3 Demo")
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Button(onClick = { navController.navigate("animation4") }) {
+            Text("Animation 4 Demo")
+        }
+
+        Spacer(modifier = Modifier.height(30.dp))
+        Text("Peter Doe", fontSize = 18.sp, fontWeight = FontWeight.Bold)
+        Text("123456445", fontSize = 16.sp, color = MaterialTheme.colors.primary)
     }
 }
